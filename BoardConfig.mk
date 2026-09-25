a191ee103cb34b1af4e44e5b87701b6fb370c7c6
Snowman-5 <92915025+Snowman-5@users.noreply.github.com>
2026-09-25 15:47:30 +0100

    Update recovery partition size and compression settings

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,3 +28,8 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TW_THEME := portrait_hdpi
 TW_INCLUDE_CRYPTO := true
 TW_NO_SCREEN_BLANK := true
+# HTC M8s recovery partition is 0x02000000 (32 MiB)
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+
+# Use stronger compression for the recovery ramdisk
+BOARD_RAMDISK_USE_LZMA := true
