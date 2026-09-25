04e06c546f8f47528c13972f2d29bd357ae393c1
Snowman-5 <92915025+Snowman-5@users.noreply.github.com>
2026-09-25 12:26:40 +0100

    Delete m8qlul-twrp-github-upload/m8qlul_twrp_device_tree directory

--- m8qlul-twrp-github-upload/m8qlul_twrp_device_tree/device.mk
+++
@@ -1,3 +0,0 @@
-LOCAL_PATH := device/htc/m8qlul
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab
--- m8qlul-twrp-github-upload/m8qlul_twrp_device_tree/omni_m8qlul.mk
+++
@@ -1,9 +0,0 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, device/htc/m8qlul/device.mk)
-$(call inherit-product, vendor/omni/config/common.mk)
-
-PRODUCT_DEVICE := m8qlul
-PRODUCT_NAME := omni_m8qlul
-PRODUCT_BRAND := htc
-PRODUCT_MODEL := HTC One M8s
-PRODUCT_MANUFACTURER := HTC
