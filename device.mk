634fe6ad4f68c3aa02e6c8d8c648fcd958ae1776
Snowman-5 <92915025+Snowman-5@users.noreply.github.com>
2026-09-26 12:13:49 +0100

    Add init.recovery.qcom.rc to PRODUCT_COPY_FILES

--- device.mk
+++ device.mk
@@ -1,3 +1,4 @@
 LOCAL_PATH := device/htc/m8qlul
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab
+    $(LOCAL_PATH)/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab \
+    $(LOCAL_PATH)/recovery/root/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc
